78c44e68bd1f092ee1f8c35265543bd5e43bf1d1
jannat337 <jannatulferdauz3@gmail.com>
2026-06-24 20:10:06 +0600

    add MongoDB connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config();
 
@@ -13,6 +14,11 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB Connected Successfully!'))
+  .catch((err) => console.log('MongoDB Connection Error:', err));
 
 // Test route
 app.get('/', (req, res) => {
